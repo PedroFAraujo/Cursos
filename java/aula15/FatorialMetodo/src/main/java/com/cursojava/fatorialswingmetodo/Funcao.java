0102fec9235b0bbaef91ac454566689327b58388
PedroFAraujo <pedrofeearaujo@gmail.com>
2023-03-06 07:26:58 -0300

    ⠀

---
+++ java/aula15/FatorialMetodo/src/main/java/com/cursojava/fatorialswingmetodo/Funcao.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.cursojava.fatorialswingmetodo;
+
+/**
+ *
+ * @author Pedro
+ */
+public class Funcao {
+    public static void main(String[] args) {
+        Fatorial fatorial = new Fatorial();
+        fatorial.setValor(6);
+        System.out.println(fatorial.getFormula() + fatorial.getFatorial());
+    }
+}
